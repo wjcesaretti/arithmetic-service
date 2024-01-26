5f5d9663d62fe25c1d232de3b3a9f9bf91c94fae
wcesaret <wcesaret@iu.edu>
2024-01-25 19:46:44 -0500

    ysy

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();// creates app server
+const cors = require('cors')
+app.use(cors())
+const port = 3000
+
+//this is an endpoint so if you send a req to web server with just a slash
+//then the req will send the resp ...
+app.get('/', (req,res) => {
+    res.send('Arithmetic service - Hello World!')
+}) 
+
+
+//will handle any http request with add and two numbers
+app.get('/add/:n/:m',(req,res) => {
+    res.json(Number(req.params.n) + Number(req.params.m))
+})
+
+
+
+app.listen(port) 
+
